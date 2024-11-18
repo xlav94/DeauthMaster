c2ae356541a7e98a4f075ad73ae7a159f7d0c668
xlav94 <cedric.guevremont@gmail.com>
2024-11-18 14:18:55 -0500

    ok

--- access_point/config/access_point.py
+++ access_point/config/access_point.py
@@ -1,8 +1,6 @@
 import subprocess
 import re
 
-ip_addr = ""
-
 def check_ip(interface):
     try:
         # Execute the `ip addr show` command for the specified interface
@@ -14,18 +12,17 @@ def check_ip(interface):
             match = re.search(r'inet (\d+\.\d+\.\d+\.\d+)', result.stdout)
             if match:
                 # Return the IP address found
-                ip_addr = match.group(1)
-                return True
+                return [True, match.group(1)]
             else:
                 # No IPv4 address found
-                return None
+                return [False, None]
         else:
             # In case of an error (e.g., interface doesn't exist)
             print(f"Error: Could not retrieve address for interface {interface}")
-            return None
+            return [False, None]
     except Exception as e:
         print(f"Error executing the command: {e}")
-        return None
+        return [False, None]
     
 def check_dnsmasq():
     try:
@@ -47,8 +44,9 @@ def check_dnsmasq():
         return False
 
 try:
-    if check_ip("wlan0"):
-        set_ip_interface = subprocess.run(["ip", "addr", "del", ip_addr, "dev", "wlan0"], check=True)
+    ip = check_ip
+    if ip[0]:
+        set_ip_interface = subprocess.run(["ip", "addr", "del", ip[1], "dev", "wlan0"], check=True)
     set_ip_interface = subprocess.run(["ip", "addr", "add", "192.168.1.1/24", "dev", "wlan0"], check=True)
     stop_dnsmasq = subprocess.run(["systemctl", "stop", "dnsmasq"], check=True) 
     stop_hostapd = subprocess.run(["systemctl", "stop", "hostapd"], check=True)
