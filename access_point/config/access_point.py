d56b121cf2302cd07a9b458ff71391c771443170
xlav94 <cedric.guevremont@gmail.com>
2024-11-17 20:44:16 -0500

    setup

--- access_point/config/access_point.py
+++ access_point/config/access_point.py
@@ -1,13 +1,26 @@
 import subprocess
 
+try: 
+    set_ip_interface = subprocess.run(["ip", "addr", "add", "192.168.1.1/24", "dev", "wlan0"], check=True)
+    stop_dnsmasq = subprocess.run(["systemctl", "stop", "dnsmasq"], check=True) 
+    stop_hostapd = subprocess.run(["systemctl", "stop", "hostapd"], check=True)
 
-ip addr add 192.168.1.1/24 dev wlan0
-systemctl stop dnsmasq  
-systemctl stop hostapd
+    subprocess.run(["iptables", "-F"], check=True)
+    subprocess.run(["iptables", "-t", "nat", "-F"], check=True)
+    subprocess.run(["iptables", "-X"], check=True)
+    subprocess.run(["iptables", "-t", "nat", "-X"], check=True)
+    subprocess.run(
+                ["iptables", "-t", "nat", "-A", "PREROUTING", "-i", "wlan0", "-p", "tcp", "--dport", "80", "-j", "DNAT", "--to-destination", "192.168.56.128:80"],
+                check=True
+            )
+    subprocess.run(["iptables", "-t", "nat", "-A", "POSTROUTING", "-j", "MASQUERADE"], check=True)
 
-iptables -F 
-iptables -t nat -A PREROUTING -i wlan0 -p tcp --dport 80 -j DNAT --to-destination 192.168.56.128:80
-iptables -t nat -A POSTROUTING -j MASQUERADE   
-
-dnsmasq -C dnsmasq.conf
-hostapd hostapd.conf
+    subprocess.run(["dnsmasq", "-C", "dnsmasq.conf"], check=True)
+    subprocess.run(["hostapd", "hostapd.conf"], check=True)
+    
+    print("All commands were executed successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"Error while executing the command: {e.cmd}")
+    print(f"Return code: {e.returncode}")
+except FileNotFoundError as e:
+    print(f"File not found: {e.filename}")
